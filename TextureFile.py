f0b81bca8a0d235b33c8d2d02bdc4c28d175c514
manoftheponcho <manoftheponcho@gmail.com>
2014-07-10 13:34:46 -0700

    cleanup

--- TextureFile.py
+++ TextureFile.py
@@ -12,7 +12,7 @@ class TextureFile:
         self._rights = self._data & 15
         #then interleave them into the full array
         self.full = numpy.dstack((self._lefts, self._rights)).flatten()
-        self.full.resize((256, 1024))
+        self.full.resize((1024, 256))
         self.full = self.full.T
 
 if __name__ == "__main__":
@@ -28,16 +28,15 @@ if __name__ == "__main__":
     random_tex_file = 'MAP001.8'
     texture_file = TextureFile(random_tex_file)
     #create an image and fill its color table with shades of grey
-    grey_scaled = QtGui.QImage(QtCore.QSize(1024, 256), QtGui.QImage.Format_Indexed8)
+    grey_scaled = QtGui.QImage(QtCore.QSize(256, 1024), QtGui.QImage.Format_Indexed8)
     grey_scaled.setColorTable([QtGui.qRgb(i * 16, i * 16, i * 16) for i in range(16)])
     for pos, color in numpy.ndenumerate(texture_file.full):
-        grey_scaled.setPixel(QtCore.QPoint(*pos), color)
+        grey_scaled.setPixel(QtCore.QPoint(pos[0], pos[1]), color)
 
     class TestWidget(QtGui.QWidget):
         def __init__(self, parent=None):
             super().__init__(parent)
         def paintEvent(self, event):
-#            pixmap = QtGui.QPixmap.fromImage(grey_scaled)
             painter = QtGui.QPainter(self)
             painter.drawImage(self.rect(), grey_scaled)
         def keyPressEvent(self, event):
@@ -45,7 +44,8 @@ if __name__ == "__main__":
                 self.close()
             else:
                 super().keyPressEvent(event)
-
+        def sizeHint(self):
+            return QtCore.QSize(256,1024)
     app = QtGui.QApplication(sys.argv)
     window = TestWidget()
     window.show()

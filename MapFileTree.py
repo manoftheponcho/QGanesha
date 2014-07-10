bc203b68a1972f301af8e1c5bef96cce9b7fc6d0
manoftheponcho <manoftheponcho@gmail.com>
2014-07-10 09:27:21 -0700

    someday I will do this without dictionaries

--- MapFileTree.py
+++ MapFileTree.py
@@ -1,6 +1,7 @@
 __author__ = 'DUDE'
 
 import os
+import numpy
 from GNSFile import GNSFile
 
 class MapFileTree:
@@ -17,15 +18,44 @@ class MapFileTree:
                           os.path.splitext(res)[1].lower() != '.gns']
         #sort the extensions by numeric order instead of lexicographic(remember to remove the dot)
         self.resources.sort(key=lambda s: int(os.path.splitext(s)[1].strip('.')))
-        self.items = dict(zip(self.gns_file.situations, self.resources))
+        #note to self:  numpy and dictionaries don't play nicely together
+        self.items = dict(zip(self.gns_file.situations.astype(tuple).tolist(), self.resources))
 
     def get_files(self, situation):
-        #return all values of self.items for which some or all parts of the key match, if the rest are zero
-        return [self.items[sit] for sit in self.items]
-#                if (sit == situation or
-#                    (sit[0], sit[1], 0)
+        #return all values of self.items for which some or all of the key matches, if the rest(except type) are zero
+        #some default indices are always returned
+        return ([self.items[sit] for sit in self.items if (((sit[0] == situation[0]) &
+                                                            (sit[1] == situation[1]) &
+                                                            (sit[2] == situation[2]) &
+                                                            (sit[3] == situation[3])) |
+                                                           ((sit[0] == situation[0]) &
+                                                            (sit[1] == situation[1]) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])) |
+                                                           ((sit[0] == situation[0]) &
+                                                            (sit[1] == 0) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])) |
+                                                           ((sit[0] == 0) &
+                                                            (sit[1] == 0) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])) |
+                                                          ((sit[0] == 34) &
+                                                            (sit[1] == 0) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])) |
+                                                           ((sit[0] == 48) &
+                                                            (sit[1] == 0) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])) |
+                                                           ((sit[0] == 112) &
+                                                            (sit[1] == 0) &
+                                                            (sit[2] == 0) &
+                                                            (sit[3] == situation[3])))])
+
 if __name__ == "__main__":
     import random
     current_map = MapFileTree('C:\\Users\DUDE\\PycharmProjects\\Ganesha-0.60\\FINALFANTASYTACTICS\\MAP\\',
                               'MAP{:03}'.format(random.randint(1, 119)))
     print(current_map.items)
+    print(current_map.get_files((34, 0, 128, 5889)))
